13e206d9c3e19a4a1177afcc49b05f26ce6aefd6
Carl Welch <carlwelchdesign@gmail.com>
2022-07-18 14:08:19 -0700

    clean up

--- server.js
+++ server.js
@@ -25,16 +25,6 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// simple route
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to bezkoder application." });
-// });
-
 require("./app/routes/review.routes")(app);
 
 // set port, listen for requests
